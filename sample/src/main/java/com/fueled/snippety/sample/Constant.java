9bc7b71d778fb27187ebc51ae0e8da1d930ff08c
chetan-fueled <chetan@fueled.com>
2018-01-10 22:14:23 +0530

    docs(*): add docs for custom spans

---
+++ sample/src/main/java/com/fueled/snippety/sample/Constant.java
@@ -0,0 +1,13 @@
+package com.fueled.snippety.sample;
+
+/**
+ * Copyright (c) 2017 Fueled. All rights reserved.
+ *
+ * @author chetansachdeva on 22/08/17
+ */
+
+public class Constant {
+    public static final String DEMO_FRAGMENT_TITLE = "Demo";
+    public static final String PAGE_FRAGMENT_TITLE = "Page";
+    public static final String HTML_FRAGMENT_TITLE = "Html";
+}
